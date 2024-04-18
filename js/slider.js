bd422c914bb69a668f2c4c31f0c7fe67d07844b2
ali saraee <ghialibar@gmail.com>
2024-04-18 14:53:42 +0430

    slider js update

--- js/slider.js
+++ js/slider.js
@@ -21,3 +21,26 @@ function OpenSlide(evt, SlideName,Explanation) {
 
     evt.currentTarget.className += " change";
   }
+let index = 0;
+let transitionDelay = 8000;
+
+let slideContainer = document.querySelector(".header__bottom");
+let slides = slideContainer.querySelectorAll(".slide");
+
+for (let slide of slides) {
+  slide.style.transition = `all ${transitionDelay/1000}s linear`;
+}
+
+showSlide(index);
+
+function showSlide(slideNumber) {
+  slides.forEach((slide, i) => {
+    slide.style.display = i == slideNumber ? "block" : "none";
+  });
+  index++;
+  if (index >= slides.length) {
+    index = 0;
+  }
+}
+
+setInterval (() => showSlide(index), transitionDelay);
